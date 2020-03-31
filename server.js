9e8b71091f088a56b96c8c8f39373220e06b0aef
Brandon Eng <beng713@gmail.com>
2020-03-31 13:15:33 -0700

    working pokedex

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-	res.sendFile(__dirname + "index.html");
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
@@ -16,23 +16,36 @@ app.post("/", function(req, res){
 	const urlHeader = "https://pokeapi.co/api/v2/";
 	const subHeader = "pokemon/"
 
+	// Gets the fields from the front end form
 	var pokemonName = String(req.body.pokemonName);
 
-	const url = urlHeader + subHeader + pokemonName + "/";
+	const url = urlHeader + subHeader + pokemonName;
 
-	https.get(url, function(response){
-		response.on("data", function(data){
+	console.log(url);
+
+	https.get(url, (response) => {
+		let data = '';
+
+		// A chunk of data has been recieved.
+		response.on('data', (chunk) => {
+		 data += chunk;
+		});
+
+		// The whole response has been received. Print out the result.
+		response.on('end', () => {
+
 			const pokemonData = JSON.parse(data);
 
 			const name = pokemonData.species.name; //str
 			const abilities = pokemonData.abilities; // [{name: "lightning-rod"}]
 			const pokemonNumber = pokemonData.id; // int
-			const baseStats = pokemonData.stats; // {base_stat: 90, stat{name:"speed"}}
-			const types = pokemonData.stats; // [{name: "electric"}]
+			const stats = pokemonData.stats; // {base_stat: 90, stat{name:"speed"}}
+			const types = pokemonData.types; // [{name: "electric"}]
 			const height = pokemonData.height; // int
 			const weight = pokemonData.weight; // int
 
-			res.write("<h1>#"+pokemonNumber+" " + name+"</h1>");
+			res.write("<h1>"+name+" (#"+pokemonNumber+")"+"</h1>");
+
 
 			res.write("<h3>Height</h3>");
 			res.write("<p>"+height+"</p>");
@@ -42,27 +55,30 @@ app.post("/", function(req, res){
 
 
 			res.write("<h3>Types</h3>");
-			for (type in types){
-				res.write("<p>"+type.name+"<p>");
+			res.write("<ul>");
+			for (const type of types){
+				res.write("<li>"+type.type.name+"</li>");
 			}
+			res.write("</ul>");
 
 			res.write("<h3>Abilities</h3>");
-			for (ability in abilities){
-				res.write("<p>"+ability.name+"<p>");
+			res.write("<ul>");
+			for (const ability of abilities){
+				res.write("<li>"+ability.ability.name+"</li>");
 			}
+			res.write("</ul>");
+
 
 			res.write("<h3>Base Stats</h3>");
-			for (bs in baseStats){
-				for (type in types){
-					res.write("<h6>"+bs.stat.name+"<h6>");
-					res.write("<p>"+bs.base_stat+"<p>");
-				}
+			res.write("<ul>");
+			for (const stat of stats){
+				res.write("<li>"+stat.stat.name+ ": "+stat.base_stat+"</li>");
 			}
+			res.write("</ul>");
 
 			res.send();
-
-		})
-	})
+  		});
+	});
 });
 
 app.listen(3000, function(){
