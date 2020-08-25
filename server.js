0c0a15c63cb9044e4261cc7aea79a166239eb01d
Brandon Eng <bce25@cornell.edu>
2020-08-24 17:26:27 -0700

    npm init and port

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.post("/", function (req, res) {
 
 app.use(express.static("public"));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000");
 });
