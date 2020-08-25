c3a8052b145b9d392677bd7525b073bb76209430
Brandon Eng <bce25@cornell.edu>
2020-08-24 17:22:28 -0700

    using ejs

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index", {});
 });
 
 app.post("/", function (req, res) {
@@ -33,63 +35,23 @@ app.post("/", function (req, res) {
         // The whole response has been received. Print out the result.
         response.on("end", () => {
             if (data == "Not Found") {
-                res.write("<p>Sorry, we couldn't find that Pokemon</p>");
+                res.render("pokemon", { found: false });
             } else {
                 const pokemonData = JSON.parse(data);
 
-                const name = pokemonData.species.name; //str
-                const abilities = pokemonData.abilities; // [{name: "lightning-rod"}]
-                const pokemonNumber = pokemonData.id; // int
-                const stats = pokemonData.stats; // {base_stat: 90, stat{name:"speed"}}
-                const types = pokemonData.types; // [{name: "electric"}]
-                const height = pokemonData.height; // int
-                const weight = pokemonData.weight; // int
-
-                res.write(
-                    "<h1>" + name + " (#" + pokemonNumber + ")" + "</h1>"
-                );
-
-                res.write("<h3>Height</h3>");
-                res.write("<p>" + height + "</p>");
-
-                res.write("<h3>Weight</h3>");
-                res.write("<p>" + weight + "</p>");
-
-                res.write("<h3>Types</h3>");
-                res.write("<ul>");
-                for (const type of types) {
-                    res.write("<li>" + type.type.name + "</li>");
+                const responseData = {
+                    found: true,
+                    name: pokemonData.species.name,
+                    sprite: pokemonData.sprites.front_default,
+                    abilities: pokemonData.abilities,
+                    pokemonNumber: pokemonData.id,
+                    stats: pokemonData.stats,
+                    types: pokemonData.types,
+                    height: pokemonData.height,
+                    weight: pokemonData.weight
                 }
-                res.write("</ul>");
-
-                res.write("<h3>Abilities</h3>");
-                res.write("<ul>");
-                for (const ability of abilities) {
-                    res.write("<li>" + ability.ability.name + "</li>");
-                }
-                res.write("</ul>");
-
-                res.write("<h3>Base Stats</h3>");
-                res.write("<ul>");
-                for (const stat of stats) {
-                    res.write(
-                        "<li>" +
-                        stat.stat.name +
-                        ": " +
-                        stat.base_stat +
-                        "</li>"
-                    );
-                }
-                res.write("</ul>");
+                res.render("pokemon", responseData);
             }
-
-            res.write("<FORM>");
-            res.write(
-                "<INPUT type='button' onClick='history.back(-1)' value='Search for another Pokemon'"
-            );
-            res.write("</FORM>");
-
-            res.send();
         });
     });
 });
